366a81358bdb3195e5a3217a5801f4f983bb3188
Kuval <kuval8877@gmail.com>
2026-04-06 22:27:54 +0100

    Set up Express to MySQL connection and fix Docker database config

--- pg-sd2/app/app.js
+++ pg-sd2/app/app.js
@@ -1,48 +1,31 @@
-// Import express.js
 const express = require("express");
+const path = require("path");
+const db = require("./services/db");
 
-// Create express app
-var app = express();
+const app = express();
 
-// Add static files location
-app.use(express.static("static"));
+app.use(express.static(path.join(__dirname, "..", "static")));
 
-// Get the functions in the db.js file to use
-const db = require('./services/db');
-
-// Create a route for root - /
-app.get("/", function(req, res) {
-    res.send("Hello world!");
+app.get("/", function (req, res) {
+  res.send("Hello world!");
 });
 
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
+app.get("/db_test", async function (req, res) {
+  try {
+    const rows = await db.query("SELECT 1 AS ok");
+    res.json(rows[0]);
+  } catch (err) {
+    console.error("DB connection failed:", err);
+    res.status(500).json({ error: "DB connection failed" });
+  }
 });
 
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
+app.get("/goodbye", function (req, res) {
+  res.send("Goodbye world!");
 });
 
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
+app.get("/hello/:name", function (req, res) {
+  res.send("Hello " + req.params.name);
 });
 
-// Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
-});
+module.exports = app;
