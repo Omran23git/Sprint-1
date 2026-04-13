427c0626a160d4d053210b43d389943e0d02e3d1
Your Full Name <moradalamdeen@gmail.com>
2026-04-13 12:14:43 +0100

    Added listing detail page and fixed app structure

--- pg-sd2/app/app.js
+++ pg-sd2/app/app.js
@@ -1,61 +1,43 @@
-// Import express.js
 const express = require("express");
 
-// Create express app
 var app = express();
 
-// Add static files location
 app.use(express.static("static"));
 
-// Get the functions in the db.js file to use
 const db = require('./services/db');
 const listingsModel = require('./services/listings');
-// Create a route for root - /
+
 app.get("/", function(req, res) {
     res.send("Hello world!");
 });
 
-// Create a route for testing the db
 app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
     sql = 'select * from test_table';
     db.query(sql).then(results => {
         console.log(results);
-        res.send(results)
+        res.send(results);
     });
 });
 
-// Create a route for /goodbye
-// Responds to a 'GET' request
 app.get("/goodbye", function(req, res) {
     res.send("Goodbye world!");
 });
 
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
 app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
     console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
     res.send("Hello " + req.params.name);
 });
-// your other routes...
 
 app.get('/listings/:id', async function (req, res) {
-  try {
-    const listing = await listingsModel.getListingById(req.params.id);
-    res.render('listing', { listing: listing });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error loading listing');
-  }
+    try {
+        const listing = await listingsModel.getListingById(req.params.id);
+        res.render('listing', { listing: listing });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error loading listing');
+    }
 });
 
-// Start server on port 3000
 app.listen(3000, function() {
-// Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
+    console.log('Server running at http://127.0.0.1:3000/');
 });
